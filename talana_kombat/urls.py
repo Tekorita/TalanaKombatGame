ac04698df2ad4985005ebf8bcaf9b802e7a07993
Tekorita <davidpalmalugo@gmail.com>
2024-02-18 18:46:57 -0400

    create view and config initial urls

--- talana_kombat/urls.py
+++ talana_kombat/urls.py
@@ -6,6 +6,7 @@ from django.views.generic.base import RedirectView
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
 from .users.views import UserViewSet, UserCreateViewSet
+from .views import HelloWorldView
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -16,7 +17,7 @@ urlpatterns = [
     path('api/v1/', include(router.urls)),
     path('api-token-auth/', views.obtain_auth_token),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-
+    path('hello/', HelloWorldView.as_view(), name='hello_world'),
     # the 'api-root' from django rest-frameworks default router
     # http://www.django-rest-framework.org/api-guide/routers/#defaultrouter
     re_path(r'^$', RedirectView.as_view(url=reverse_lazy('api-root'), permanent=False)),
---
+++ talana_kombat/views.py
@@ -0,0 +1,7 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+class HelloWorldView(APIView):
+    def get(self, request):
+        return Response({"message": "Hola Mundo"}, status=status.HTTP_200_OK)
